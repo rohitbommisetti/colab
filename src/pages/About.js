22c911a87ed7d3ec4c4deeefe313c56b8b973fdc
Rohit Bommisetti <rohitbommisetti3@gmail.com>
2022-09-16 14:51:32 -0400

    added about

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,17 @@ const About = (props) => {
 return (
     <div className='base'>
       Under construction
+      //  <h1> About CoLab </h1> 
+   //     <p>
+    
+         //   We are a team of students disillusioned by the discussion of entrepreneurship on campus — we know that learning is best done by doing. 
+
+//We’re talking about initiatives like a startup fair at University of Pittsburgh, software products with recurring revenue, and networking events to accelerate your college career.
+
+// We are making a bet on student potential — student builders have created this generation's most impactful products like Facebook, Dropbox, Reddit, and DoorDash. We are super bullish on what Pitt's talent can achieve. 
+    
+    
+ //       </p>
     </div>
   )
 };
